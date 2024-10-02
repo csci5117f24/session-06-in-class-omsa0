fe3df9dba893f0ecc95d344d6216c36efc5ee4c6
omsa0 <theos2014.os@gmail.com>
2024-10-02 14:49:07 -0500

    remove dotenv

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
-from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
 from os import environ as env
 from db import *
